1c711eeef0f2c570f2512a63df1e142cf01d24e7
AlexanderKurshnev <a.kurshnev@carx-tech.com>
2025-02-10 16:26:23 +0300

    add Runner for cmd

--- Assets/Tests/CharacterTests.cs
+++ Assets/Tests/CharacterTests.cs
@@ -11,11 +11,14 @@ public class CharacterTests : InputTestFixture
     GameObject _character = Resources.Load<GameObject>("Character");
     Keyboard _keyboard;
     
+    
     public override void Setup()
     {
         SceneManager.LoadScene("Scenes/SimpleTesting");
         
-        base.Setup();
+        //зачем эта хрень base.Setup()????
+        //base.Setup();
+        
         _keyboard = InputSystem.AddDevice<Keyboard>();
 
         var mouse = InputSystem.AddDevice<Mouse>();
@@ -27,8 +30,6 @@ public class CharacterTests : InputTestFixture
     public override void TearDown()
     {
         base.TearDown();
-        //Object.Destroy(_character);
-        //Object.DestroyImmediate(_character, true);
     }
     
     [UnityTest]
@@ -51,29 +52,23 @@ public class CharacterTests : InputTestFixture
         Release(_keyboard.upArrowKey);
         yield return new WaitForSeconds(1f);
         Assert.That(characterInstance.transform.GetChild(0).transform.position.z, Is.GreaterThan(1.5f));
-        
-        //Object.Destroy(characterInstance);
     }
     
     [UnityTest]
     public IEnumerator TestPlayerFallDamage()
     {
-    // spawn the character in a high enough area in the test scene
+    
         GameObject characterInstance = GameObject.Instantiate(_character, new Vector3(0f, 4f, 17.2f), Quaternion.identity);
 
-    // Get a reference to the PlayerHealth component and assert currently at full health (1f)
         var characterHealth = characterInstance.GetComponent<PlayerHealth>();
         Assert.That(characterHealth.Health, Is.EqualTo(1f));
 
-    // Walk off the ledge and wait for the fall
         Press(_keyboard.upArrowKey);
         yield return new WaitForSeconds(0.5f);
         Release(_keyboard.upArrowKey);
         yield return new WaitForSeconds(2f);
 
-    // Assert that 1 health point was lost due to the fall damage
         Assert.That(characterHealth.Health, Is.EqualTo(0.9f));
-        //Object.Destroy(characterInstance);
     }
     
 }
---
+++ Assets/Tests/Runner.cs
@@ -0,0 +1,50 @@
+using UnityEditor;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEngine;
+
+public static class Runner
+{
+    private static TestRunnerApi _runner = null;
+
+    private class MyCallbacks : ICallbacks
+    {
+
+        public void RunStarted(ITestAdaptor testsToRun)
+        {}
+
+        public void RunFinished(ITestResultAdaptor result)
+        {
+            _runner.UnregisterCallbacks(this);
+            if (result.ResultState != "Passed")
+            {
+                Debug.Log("Tests failed :(");
+                if (Application.isBatchMode)
+                    EditorApplication.Exit(1);
+            }
+            else
+            {
+                Debug.Log("Tests passed :)");
+                if (Application.isBatchMode)
+                    EditorApplication.Exit(0);
+            }
+        }
+
+        public void TestStarted(ITestAdaptor test)
+        {}
+
+        public void TestFinished(ITestResultAdaptor result)
+        {}
+    }
+
+    public static void RunUnitTests()
+    {
+        _runner = ScriptableObject.CreateInstance<TestRunnerApi>();
+        Filter filter = new Filter()
+        {
+            //testMode = TestMode.EditMode
+            testMode = TestMode.PlayMode
+        };
+        _runner.RegisterCallbacks(new MyCallbacks());
+        _runner.Execute(new ExecutionSettings(filter));
+    }
+}
